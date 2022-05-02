930129b96f9ee47a3926de3fb47311a2a9841f4c
Luciano <barrabinoCompany@gmail.com>
2022-05-02 18:05:41 -0300

    react-create-app on Heroku

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,54 +1,14 @@
 import React from 'react'
 import "./portfolio.css"
-import IMG1 from "../../assets/portfolio1.jpg"
-import IMG2 from "../../assets/portfolio2.jpg"
-import IMG3 from "../../assets/portfolio3.jpg"
-import IMG4 from "../../assets/portfolio4.jpg"
-import IMG5 from "../../assets/portfolio5.png"
-import IMG6 from "../../assets/portfolio6.jpg"
+import IMG1 from "../../assets/Captura.JPG"
 
 const data = [
   {
     id: 1, 
     image : IMG1,  
-    title: 'Zero Gravity' , 
-    github: 'https://github.com' , 
-    demo: 'https://dribbble.com/shots/17907372-Zero-Gravity'
-  },
-  {
-    id: 2, 
-    image : IMG2,  
-    title: 'GitHub - Dark mode colors', 
-    github: 'https://github.com' , 
-    demo: 'https://dribbble.com/shots/15487845-GitHub-Dark-mode-colors'
-  },
-  {
-    id: 3, 
-    image : IMG3,  
-    title: 'Crypto Web!' , 
-    github: 'https://github.com' , 
-    demo: 'https://dribbble.com/shots/17905027-Crypto-Web '
-  },
-  {
-    id: 4, 
-    image : IMG4,  
-    title: 'Ethereum Web' , 
-    github: 'https://github.com' , 
-    demo: 'https://dribbble.com/shots/17760205-Ethereum-Web '
-  },
-  {
-    id: 5, 
-    image : IMG5,  
-    title: 'Nft Web' , 
-    github: 'https://github.com' , 
-    demo: 'https://dribbble.com/shots/17745852-Nft-Web '
-  },
-  {
-    id: 6, 
-    image : IMG6,  
-    title: 'Apollo icon' , 
-    github: 'https://github.com' , 
-    demo: 'https://dribbble.com/shots/17900422-Apollo-icon '
+    title: 'Tenzies Game with React' , 
+    github: 'https://github.com/Luciano2324/Tenzie_Game' , 
+    demo: 'https://calm-spire-05534.herokuapp.com/'
   }
 ]
 
