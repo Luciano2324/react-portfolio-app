3f7aa979c9dcdd6a48346d292f753c79ae9c9903
Luciano <barrabinoCompany@gmail.com>
2022-05-07 15:38:16 -0300

    react-create-app on Heroku

--- src/index.js
+++ src/index.js
@@ -8,3 +8,8 @@ const container = document.getElementById("root")
 const root = ReactDOMClient.createRoot(container)
 
 root.render(<App />)
+
+// TO UPDATE WRITE THIS
+//git init
+// git commit -m "react-create-app on Heroku"
+// git push heroku master
